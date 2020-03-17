814922c90a3046ac591bf00967a7cca1bb4ee38a
Bohdan <bohdanimiela@gmail.com>
2020-03-17 11:58:29 +0100

    Add request submission summary display

--- src/components/modal/summary/index.jsx
+++ src/components/modal/summary/index.jsx
@@ -7,10 +7,57 @@ import ModalControls from '../shared/ModalControls';
 const ModalSummary = ({ onClose, data }) => {
   const closeModal = () => onClose();
 
+  const generateScheduleField = schedule => {
+    switch (schedule) {
+      case 'date':
+        return (
+          <div>
+            <span className='summary__field-label'>Scheduled for</span>
+            {data.date}
+            <span className='summary__field-label'>at</span>
+            {data.hour}hr
+          </div>
+        );
+      case 'daily':
+        return (
+          <div>
+            <span className='summary__field-label'>
+              Scheduled for everyday at
+            </span>
+            {data.hour}hr
+          </div>
+        );
+      case 'weekly':
+        return (
+          <div>
+            <span className='summary__field-label'>Scheduled for every</span>
+            {data.day}
+            <span className='summary__field-label'>at</span>
+            {data.hour}hr
+          </div>
+        );
+      default:
+        return;
+    }
+  };
+
   return (
     <>
-      <ModalHeader title='Request Summary' />
-      <div>{data.name}</div>
+      <ModalHeader title='Request Submitted!' />
+      <div>
+        <span className='summary__field-label'>Report name</span>
+        {data.name}
+      </div>
+      <div>
+        <span className='summary__field-label'>Format</span>
+        {data.format}
+      </div>
+      <div>
+        <span className='summary__field-label'>Send to</span>
+        {data.email}
+      </div>
+      {generateScheduleField(data.schedule)}
+
       <ModalControls>
         <ModalControlButton buttonText='OK' handleClick={closeModal} />
       </ModalControls>
