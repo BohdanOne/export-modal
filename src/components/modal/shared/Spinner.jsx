74aed564f83af63912b67f60244ce5989cdbbb83
Bohdan <bohdanimiela@gmail.com>
2020-03-17 17:00:14 +0100

    Add spinner

--- src/components/modal/shared/Spinner.jsx
+++ src/components/modal/shared/Spinner.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Spinner = ({ message }) => {
-  return <div className='spinner'>{message}</div>;
+  return (
+    <div className='spinner'>
+      <p className='spinner__message'>{message}</p>
+      <div className='spinner__wrapper'>
+        <div className='spinner__animation'></div>
+      </div>
+    </div>
+  );
 };
 
 Spinner.propTypes = {
